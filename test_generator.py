685e3732f939a58c5fddaa307b917f98b4ad5ca8
Kalpa Welivitigoda <callkalpa@gmail.com>
2012-08-06 18:46:09 +0530

    database table check, params for functions modified, connection string from db.py

--- test_generator.py
+++ test_generator.py
@@ -7,38 +7,59 @@ from db import *
 def connect_db():
 	global cur
 	db = MySQLdb.connect(host='localhost',user='freelab',passwd='freelab',db='freelab')
+	#db = MySQLdb.connect(get_connection_string())
 	cur = db.cursor()
 
-def check_for_report(fi):
+def check_for_report():
 	return True
 
-def generate_report(fi):
+def generate_report():
 	pass
 
-def check_for_gui(fi):
+def check_for_gui():
 	return True
 
-def generate_gui(fi):
+def generate_gui():
 	pass
 
-def check_for_table(fi):
+def check_for_table():
+	sql = 'SELECT COUNT(*) FROM information_schema.tables WHERE table_schema="[' + get_database() + ']" AND table_name="[' + data[0] + ']";'
+	cur.execute(sql)
+	
+	if cur.fetchone()[0] > 0:
+		return True;
 	return False
 
-def generate_table(fi):
+def generate_table():
 	pass
+
+def decode_test_definition(fi):
+	global data
+	f = open(fi,'r')
+	data = []
+
+	for line in f.readlines():
+		data.append(line.replace('\n',''))
+	
+	f.close()
+
+	for a in data:
+		print a
 
 def main():
 	connect_db()
 
 	for fi in sys.argv[1:]:
-		if not check_for_table(fi):
-			generate_table(fi)
+		decode_test_definition(fi)
 
-		if not check_for_gui(fi):
-			generate_gui(fi)
+		if not check_for_table():
+			generate_table()
 
-		if not check_for_report(fi):
-			generate_report(fi)
+		if not check_for_gui():
+			generate_gui()
+
+		if not check_for_report():
+			generate_report()
 			
 if __name__ == '__main__':
 	main()
