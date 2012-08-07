9dbaf09138388212adebde54366887ee0f9305aa
Kalpa Welivitigoda <callkalpa@gmail.com>
2012-08-07 20:05:29 +0530

    removed database connection function modified check_for_table() implemented get_table_value()

--- test_generator.py
+++ test_generator.py
@@ -1,14 +1,7 @@
 # This script generates the mysql table, user interface and the report for the specified test definition files
 
 import sys
-import MySQLdb
-from db import *
-
-def connect_db():
-	global cur
-	db = MySQLdb.connect(host='localhost',user='freelab',passwd='freelab',db='freelab')
-	#db = MySQLdb.connect(get_connection_string())
-	cur = db.cursor()
+import db
 
 def check_for_report():
 	return True
@@ -23,15 +16,44 @@ def generate_gui():
 	pass
 
 def check_for_table():
-	sql = 'SELECT COUNT(*) FROM information_schema.tables WHERE table_schema="[' + get_database() + ']" AND table_name="[' + data[0] + ']";'
-	cur.execute(sql)
-	
-	if cur.fetchone()[0] > 0:
+	sql = 'SELECT COUNT(*) FROM information_schema.tables WHERE table_schema="' + db.get_database() + '" AND table_name="' + db.validate(data[0]) + '";'
+	cur = db.execute_sql(sql)
+	tmp = cur.fetchone()[0]
+	if tmp > 0:
+		print 'table \'' + data[0] + '\' already exists'
 		return True;
 	return False
 
+# returns the appropriate sql table field type
+def get_table_field(value):
+	if value is 'T':
+		return 'VARCHAR(12)'
+	if value is 'I':
+		return 'INT'
+	if value is 'F':
+		return 'FLOAT'
+	if value is 'A':
+		return 'VARCHAR(50)'	
+
 def generate_table():
-	pass
+	sql = ('CREATE TABLE `' + db.validate(data[0]) + '` (\n' + 
+	'`id` INT NOT NULL ,\n' +
+  	'`patient_id` VARCHAR(12) NULL ,\n')
+
+	for test in data[1:]:
+		tmp = test.split(';')
+		test_name = db.validate(tmp[0])
+		value = get_table_field(tmp[1])
+		sql = sql + '`' + test_name + '` ' + value + ' NULL , \n'
+
+
+	sql = sql + ('`user` VARCHAR(2) NULL , \n'
+	'`status` VARCHAR(10) NULL , \n'
+	'`time_stamp` DATE NULL)')
+
+
+	db.execute_sql(sql)
+	print '\'' + data[0] + '\' table created'
 
 def decode_test_definition(fi):
 	global data
@@ -43,12 +65,7 @@ def decode_test_definition(fi):
 	
 	f.close()
 
-	for a in data:
-		print a
-
 def main():
-	connect_db()
-
 	for fi in sys.argv[1:]:
 		decode_test_definition(fi)
 
