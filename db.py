0a9aa25809e086906400038fcd077947e385763a
Kalpa Welivitigoda <callkalpa@gmail.com>
2012-08-07 18:55:17 +0530

    reimplemented connecting to the database

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+import MySQLdb
 
 DBFILE='db.config'
 settings = {}
@@ -5,36 +6,25 @@ settings = {}
 def read_config_file():
 	f = open(DBFILE,'r')
 	
-
 	for line in f.readlines():
 		temp = line.replace('\n','').split(':')
 		settings[temp[0]] = temp[1]
 
 	f.close()
 
-def get_connection_string():
-	read_config_file()
-	# return with the single quotes trimmed
-	return (','.join(["%s=%s" %(k,v) for k,v in settings.items()]))[1:-1]
-
 def get_database():
 	read_config_file()
 	return settings['db']
 	
-def get_connection_string2():
-	f = open(DBFILE,'r')
-	
-	temp = []
-	
-	for line in f.readlines():
-		temp.append(line.replace('\n',''))
-
-	f.close()
-	return ','.join(temp)
+def execute_sql(sql):
+	read_config_file()
+	db = MySQLdb.connect(**settings)
+	cur = db.cursor()
+	cur.execute(sql)
+	return cur
 
 def main():
-	read_config_file()
-	get_database()
+	pass
 
 if __name__ == '__main__':
 	main()
