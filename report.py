dc04ef16ea2f32169a1b314bd2ec4dc57639cc4c
Kalpa Welivitigoda <callkalpa@gmail.com>
2012-08-23 22:24:03 +0530

    report height calculation margins set column widths in fields set

--- report.py
+++ report.py
@@ -3,6 +3,8 @@ from pyjon.reports import ReportFactory
 
 output = []
 fields_list = []
+REPORT_WIDTH = 7.5 # in inches
+REPORT_HEIGHT = 0
 
 # generates the report of the given file with given data (data is a list)
 def generate_report(fi, data):
@@ -20,6 +22,7 @@ def generate_report(fi, data):
 
 	# prepare fields list
 	fields_list = temp[1:]
+	increase_report_height(0.25 * len(fields_list))
 
 	if len(temp) > 2:
 		output.append(get_title(temp[0]))
@@ -28,13 +31,18 @@ def generate_report(fi, data):
 
 	write_xml()
 
+def increase_report_height(height):
+	global REPORT_HEIGHT
+	REPORT_HEIGHT += height
+
 # title of the test (for tests with multiple fields)
 def get_title(title):
+	increase_report_height(0.75)
 	return '<h1>' + title + '</h1>\n'
 
 def get_fields(fields):
-	temp = """<blockTable repeatRows="1">
-    <tr><td py:for="i in range(4)">Kalpa ${i}</td></tr>
+	temp = """<blockTable repeatRows="1" alignment="left" colWidths="40% 20% 20% 20%">
+    <tr><td py:for="i in range(4)"></td></tr>
     <tr py:for="line in data"><td py:for="col in line.split(';')" py:content="col" /></tr>
   </blockTable>"""
 
@@ -43,21 +51,23 @@ def get_fields(fields):
 # writes the xml output of the report
 def write_xml():
 	global output
+	global REPORT_WIDTH
+	global REPORT_HEIGHT
 
 	f = open('temp.xml','w')
 	
 	# header of the xml
-	f.write("""<?xml version="1.0" encoding="iso-8859-1" standalone="no" ?>
+	f.write('''<?xml version="1.0" encoding="iso-8859-1" standalone="no" ?>
 <!DOCTYPE document SYSTEM "rml_1_0.dtd">
 <document xmlns:py="http://genshi.edgewall.org/">
-<template pageSize="(595, 842)" leftMargin="72" showBoundary="0">
+<template leftMargin="0.5in" rightMargin="0.5in" showBoundary="1" pageSize="A3">
   <pageTemplate id="main">
-    <frame id="first" x1="1in" y1="1in" width="6.27in" height="9.69in"/>
+    <frame id="first" x1="0.5in" y1="0.5in" width="''' + str(REPORT_WIDTH) + '''in" height="''' + str(REPORT_HEIGHT) + '''in" showBoundary="1"/>
   </pageTemplate>
 </template>
 <stylesheet>
 </stylesheet>
-<story>""")
+<story>''')
 
 	# write title and fields
 	temp = ''
