0b0a681103344d2a6f86ca04b21f7d7bf816d4f8
Kalpa Welivitigoda <callkalpa@gmail.com>
2012-09-08 19:49:28 +0530

    made this into a class test data is passes as a dictionary from the database adjusted row heights of title and tests implemented fixed column widths

--- report.py
+++ report.py
@@ -1,110 +1,110 @@
 import sys
+import db
+import test_fields
 from pyjon.reports import ReportFactory
 
-output = []
-fields_list = []
-REPORT_WIDTH = 7.5 # in inches
-REPORT_HEIGHT = 0
 
-# generates the report of the given file with given data (data is a list)
-def generate_report(fi, data):
-	global output
-	global fields_list
+# generates the report of the given file with given data (data is a dictionary)
+class Report:
 
-	f = open(fi,'r')
+	output = []
+	fields_list = []
+	REPORT_WIDTH = 540 # in points (7.5 in inches)
+	REPORT_HEIGHT = 0 # in points
+	LEFT_MARGIN = 36
+	RIGHT_MARGIN = 36
+	# frame width = REPORT_WIDTH - (LEFT_MARGIN + RIGHT_MARGIN)
 
-	temp = []
+	test_field = None
 
-	for line in f.readlines():
-		temp.append(line.replace('\n',''))
+	data = {} # holds the dict with test results
+
+	def generate_report(self):
+		temp_from_definition = self.test_field.get_test_name_and_fields() # fields read from the test definition file (no test result values)
+
+		# replace field type with values and prepare fields list
+		for t in temp_from_definition[1:]:
+			tem = t.split(';')
+			tem[1] = str(self.data[db.validate(tem[0])])
+			self.fields_list.append(';'.join(tem))
+
+		self.increase_report_height(24 * len(self.fields_list))
+
+		if len(self.fields_list) > 1:
+			self.output.append(self.get_title(temp_from_definition[0]))
 	
-	f.close()
+		self.output.append(self.get_fields(self.fields_list))
 
-	# prepare fields list
-	fields_list = temp[1:]
-	increase_report_height(0.25 * len(fields_list))
+		self.write_xml()
 
-	if len(temp) > 2:
-		output.append(get_title(temp[0]))
+	def increase_report_height(self, height):
+		self.REPORT_HEIGHT += height
+
+	# title of the test (for tests with multiple fields)
+	def get_title(self, title):
+		self.increase_report_height(28) # title in 14p
+		return '<para style="report_title">' + title + '</para>\n'
+
+	def get_fields(self, fields):
+		temp = """<blockTable style="fields" repeatRows="1" alignment="left" colWidths="234 140.4 46.8 46.8">
+	    <tr><td py:for="i in range(4)"></td></tr>
+	    <tr py:for="line in data"><td py:for="col in line.split(';')" py:content="col" /></tr>
+	  </blockTable>"""
+
+		return temp
 	
-	output.append(get_fields(temp[1:]))
+	# writes the xml output of the report
+	def write_xml(self):
 
-	write_xml()
-
-def increase_report_height(height):
-	global REPORT_HEIGHT
-	REPORT_HEIGHT += height
-
-# title of the test (for tests with multiple fields)
-def get_title(title):
-	increase_report_height(0.75)
-	return '<para style="report_title"><i>' + title + '</i></para>\n'
-
-def get_fields(fields):
-	temp = """<blockTable style="fields" repeatRows="1" alignment="left" colWidths="40% 20% 20% 20%">
-    <tr><td py:for="i in range(4)"></td></tr>
-    <tr py:for="line in data"><td py:for="col in line.split(';')" py:content="col" /></tr>
-  </blockTable>"""
-
-	return temp
-
-# writes the xml output of the report
-def write_xml():
-	global output
-	global REPORT_WIDTH
-	global REPORT_HEIGHT
-
-	f = open('temp.xml','w')
+		f = open('temp.xml','w')
 	
-	# header of the xml
-	f.write('''<?xml version="1.0" encoding="iso-8859-1" standalone="no" ?>
-<!DOCTYPE document SYSTEM "rml_1_0.dtd">
-<document xmlns:py="http://genshi.edgewall.org/">
-<docinit>
-	<registerTTFont faceName="Garamond" fileName="font/Garamond.ttf"/>
-	<registerTTFont faceName="Garamond-bold" fileName="font/Garamonb.ttf"/>
-</docinit>
-<template leftMargin="0.5in" rightMargin="0.5in" showBoundary="1" pageSize="A3">
-  <pageTemplate id="main">
-    <frame id="first" x1="0.5in" y1="0.5in" width="''' + str(REPORT_WIDTH) + '''in" height="''' + str(REPORT_HEIGHT) + '''in" showBoundary="1"/>
-  </pageTemplate>
-</template>
-<stylesheet>
-	<paraStyle name="report_title" fontName="Garamond-bold" fontSize="14"/>
-	<blockTableStyle id="fields">
-		<!-- This sets a font for the second row -->
-		<blockFont name="Garamond" size="12" start="0,0" stop="-1,-1"/>
-		<blockFont name="Garamond-bold" size="12" start="1,1" stop="1,-1" />
-	</blockTableStyle>
-</stylesheet>
-<story>''')
+		# header of the xml
+		f.write('''<?xml version="1.0" encoding="iso-8859-1" standalone="no" ?>
+	<!DOCTYPE document SYSTEM "rml_1_0.dtd">
+	<document xmlns:py="http://genshi.edgewall.org/">
+	<docinit>
+		<registerTTFont faceName="Garamond" fileName="font/Garamond.ttf"/>
+		<registerTTFont faceName="Garamond-bold" fileName="font/Garamonb.ttf"/>
+	</docinit>
+	<template leftMargin="''' + str(self.LEFT_MARGIN)  + '''" rightMargin="''' + str(self.RIGHT_MARGIN)  + '''" showBoundary="1" pageSize="A4">
+	  <pageTemplate id="main">
+	    <frame id="first" x1="0.5in" y1="0.5in" width="''' + str(self.REPORT_WIDTH) + '''" height="''' + str(self.REPORT_HEIGHT) + '''" showBoundary="1"/>
+	  </pageTemplate>
+	</template>
+	<stylesheet>
+		<paraStyle name="report_title" fontName="Garamond-bold" fontSize="14"/>
+		<blockTableStyle id="fields">
+			<blockFont name="Garamond" size="12" start="0,0" stop="-1,-1"/>
+			<blockFont name="Garamond-bold" size="12" start="1,1" stop="1,-1" />
+		</blockTableStyle>
+	</stylesheet>
+	<story>''')
 
-	# write title and fields
-	temp = ''
+		# write title and fields
+		temp = ''
 
-	for line in output:
-		temp += line
+		for line in self.output:
+			temp += line
 
-	f.write(temp)
+		f.write(temp)
 
-	# footer of the xml
-	f.write("""</story>
-</document>
-""")
+		# footer of the xml
+		f.write("""</story>
+	</document>
+	""")
 
-	f.close()
+		f.close()
 	
-	write_pdf('temp.xml')
+		self.write_pdf('temp.xml')
 
-def write_pdf(fi):
-	factory = ReportFactory()
-	factory.render_template(template_file=fi, data=fields_list)
+	def write_pdf(self, fi):
+		factory = ReportFactory()
+		factory.render_template(template_file=fi, data=self.fields_list)
 
-	factory.render_document('test.pdf')
-	factory.cleanup()
+		factory.render_document('test.pdf')
+		factory.cleanup()
 	
-def main():
-	generate_report(sys.argv[1], '')
+	def __init__(self, test_definition_file, data):
+		self.test_field = test_fields.TestField(test_definition_file)
+		self.data = data
 
-if __name__ == '__main__':
-	main()
