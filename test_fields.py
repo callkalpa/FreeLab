445c7e9f621400e390c34c6b53aed249d2e655f2
Kalpa Welivitigoda <callkalpa@gmail.com>
2012-09-28 21:43:10 +0530

    issue with appending elements twice to output list fixed

--- test_fields.py
+++ test_fields.py
@@ -6,6 +6,7 @@ class TestField():
 	output = []
 
 	def __init__(self, test_definition_file):
+		self.output = []
 		f = open(test_definition_file,'r')
 		
 		for line in f.readlines():
@@ -13,8 +14,9 @@ class TestField():
 
 	# return the list of fields including the test name
 	def get_test_name_and_fields(self):
-		l = len(self.output)
-		return self.output[:l/2]
+		return self.output
+#		l = len(self.output)
+#		return self.output[:l/2]
 
 	# return test name
 	def get_test_name(self):
