eba2c5c801c9a4c88e3b0fa0716168bf49da213b
Kalpa Welivitigoda <callkalpa@gmail.com>
2012-09-29 14:11:07 +0530

    renamed

--- main.py
+++ main.py
@@ -1,35 +1,92 @@
-# generates the main, patient sql tables and main gui
+# displays the main window
 
+from gi.repository import Gtk
+import sys
 import db
+import datetime
+import re
+
+builder = Gtk.Builder()
+patient_id = ''
+
+class Handler:
+	global builder
+	global patient_id
+
+	def onDeleteWindow(self, *args):
+		Gtk.main_quit(*args)
+
+	def search():
+		pass
+
+	def add_patient():
+		pass
+
+	def add():
+		pass
+
+	def print_all():
+		pass
+
+	def print_selected():
+		pass
+
+	def preview():
+		pass
+
+	def done(self, button):
+		global values
+		fields = test_field.get_gui_field_list()
+
+		values['`id`'] = '0' # auto increment value
+		values['`time_stamp`'] = "'" + str(datetime.datetime.now()) + "'"
+
+		for field in fields:
+			obj =  builder.get_object(field)
+
+			key = '`' + field + '`'			
+
+			if obj == None:
+				# calculation field
+				values[key] = "'" + str(get_calculation_result(field)) + "'" # result of the calculation 
+				continue
+
+			object_type = obj.get_name()
+			
+			if object_type == 'GtkEntry':
+				values[key] = "'" + get_gtkEntry_value(obj) + "'"
+			elif object_type == 'GtkTextView':
+				values[key] = "'" + get_gtkTextView_value(obj) + "'"
+			elif object_type == 'GtkComboBoxText':
+				values[key] = "'" + get_gtkComboBoxText_value(obj) + "'"
+
+		# calls sql insert command with test definition file name, fields and values
+		db.feed_test_data(db.validate(builder.get_object('title').get_text()), values)	
+
+def get_gtkEntry_value(obj):
+	return obj.get_text()
+
+def get_gtkTextView_value(obj):
+	buf = obj.get_buffer()
+	return buf.get_text(buf.get_start_iter(), buf.get_end_iter(), False)
+
+def get_gtkComboBoxText_value(obj):
+	temp = obj.get_active_text()
+	if temp == None:
+		return ''
+	return temp
 
 def main():
-	create_tables()
-	create_gui()
+	global builder
+	
+	builder.add_from_file('gui/main.glade')
+	builder.connect_signals(Handler())
 
-def create_gui():
-	pass
-
-def create_tables():
-	# create patient table if it does not exist
-	patient_sql = '''CREATE TABLE IF NOT EXISTS `patient`(
-`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-`patient_id` VARCHAR(10) NULL,
-`name` VARCHAR(20) NULL,
-`user` VARCHAR(10) NULL
-)'''
-	db.execute_sql(patient_sql)
-
-	# create main table if it does not exist
-	main_sql = '''CREATE TABLE IF NOT EXISTS `main`(
-`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-`patient_id` VARCHAR(10) NULL,
-`requested_bd` VARCHAR(30) NULL,
-`sample_co` DATE NULL,
-`test_id` INT NULL,
-`data_entered` TIMESTAMP NULL,
-`printed` TIMESTAMP NULL
-)'''
-	db.execute_sql(main_sql)
+	window = builder.get_object("main")
+	window.show_all()
+	
+	Gtk.main()
 
 if __name__ == '__main__':
 	main()
+
