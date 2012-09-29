a54074f2abe125a24ce1a47b0a64b66f29667271
Kalpa Welivitigoda <callkalpa@gmail.com>
2012-09-29 09:28:34 +0530

    renamed to main_generator.py

---
+++ main_generator.py
@@ -0,0 +1,44 @@
+# generates the main, patient sql tables and main gui
+
+import db
+
+def main():
+	create_tables()
+	create_gui()
+
+def create_gui():
+	pass
+
+def create_tables():
+	# create patient table if it does not exist
+	patient_sql = '''CREATE TABLE IF NOT EXISTS `patient`(
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`patient_id` VARCHAR(10) NULL,
+`name` VARCHAR(20) NULL,
+`user` VARCHAR(10) NULL
+)'''
+	db.execute_sql(patient_sql)
+
+	# create main table if it does not exist
+	main_sql = '''CREATE TABLE IF NOT EXISTS `main`(
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`patient_id` VARCHAR(10) NULL,
+`requested_bd` VARCHAR(30) NULL,
+`sample_co` DATE NULL,
+`test_id` INT NULL,
+`data_entered` TIMESTAMP NULL,
+`printed` TIMESTAMP NULL
+)'''
+	db.execute_sql(main_sql)
+
+	# create test_info table
+	test_info = ''' CREATE TABLE IF NOT EXISTS `test_info`(
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`test_id` INT,
+`definition_file` VARCHAR(30) NULL,
+`gui_file` VARCHAR(40) NULL
+)'''	
+	db.execute_sql(test_info)
+
+if __name__ == '__main__':
+	main()
