42a1a478193c9737896c86ed196a3b6281274fd9
Kalpa Welivitigoda <callkalpa@gmail.com>
2012-10-06 12:52:57 +0530

    main report is printed before test reports

--- generate_report.py
+++ generate_report.py
@@ -11,12 +11,13 @@ def gen_report(patient_id, test_definition_files):
 
 	rep = report.Report()
 
+	rep.set_main_data_list(db.get_main_report_list(patient_id))
+	rep.generate_main()
+
 	for test_definition_file in test_definition_files:
 		temp = test_fields.TestField(test_definition_file)
 		test_name = db.validate(temp.get_test_name())
-
 		cur = db.retrive_test_data(test_name, patient_id)
-
 		data = cur.fetchone()
 		rep.set_test_info(test_definition_file, data)
 		rep.generate_report()
