e811287d8257501fb286d25611058f3233cc1a6a
Kalpa Welivitigoda <callkalpa@gmail.com>
2012-10-09 21:11:11 +0530

    modified according to the modified retrive_test_data() in db.py

--- generate_report.py
+++ generate_report.py
@@ -17,8 +17,7 @@ def gen_report(patient_id, test_definition_files):
 	for test_definition_file in test_definition_files:
 		temp = test_fields.TestField(test_definition_file)
 		test_name = db.validate(temp.get_test_name())
-		cur = db.retrive_test_data(test_name, patient_id)
-		data = cur.fetchone()
+		data = db.retrive_test_data(test_name, patient_id)
 		rep.set_test_info(test_definition_file, data)
 		rep.generate_report()
 
